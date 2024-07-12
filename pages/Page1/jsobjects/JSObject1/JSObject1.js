e0377f62ebc6e4c661539901b34246b36c08c21d
kamakshi <kamakshi@appsmith.com>
2024-07-12 13:26:52 +0000

    a

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,13 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+		return QueryModule11.run({ input1: "2" })
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
